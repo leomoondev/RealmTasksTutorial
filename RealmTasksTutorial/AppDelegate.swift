b300e26b6327f8c3786ac87052814d5c7497af1c
Leo Moon <leomoondev@gmail.com>
2017-04-15 23:31:14 -0700

    first commit

---
+++ RealmTasksTutorial/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RealmTasksTutorial
+//
+//  Created by Hyung Jip Moon on 2017-04-15.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController(style: .plain))
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ RealmTasksTutorial/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RealmTasksTutorial
+//
+//  Created by Hyung Jip Moon on 2017-04-15.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+
+
+}
+
